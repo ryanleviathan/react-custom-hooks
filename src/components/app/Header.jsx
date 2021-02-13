d66cceac59e0a3ff1eec88e96eed7616b65a5836
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-12 16:24:07 -0800

    finished

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import history from '../app/history';
 
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <Link to={history.goBack()}>
-          <button>back</button>
-        </Link>
-        <Link to={history.goForward()}>
-          <button>forward</button>
-        </Link>
         <Link to={'/'}>
           <button>home</button>
         </Link>
--- src/containers/AllQuotes.jsx
+++ src/containers/AllQuotes.jsx
@@ -7,14 +7,19 @@ const AllQuotes = () => {
   const [quotes, setQuotes] = useState([]);
 
   useEffect(() => {
-    findQuotes().then((quotes) => {
-      setQuotes(quotes);
-      setLoading(false);
-    });
+    findQuotes()
+      .then((quotes) => {
+        setQuotes(quotes);
+        setLoading(false);
+      });
   }, []);
   const gif = 'https://giphy.com/gifs/mashable-3oEjI6SIIHBdRxXI40';
   if(loading) return <img src={gif} alt="Loading" />;
-  return <QuoteList quotes={quotes} />;
+  return (
+    <>
+      <QuoteList quotes={quotes} />
+    </>
+  );
 };
 
 export default AllQuotes;
