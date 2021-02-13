b2df28015d47607f33cfe73cd80e190e52127dd7
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-12 16:40:11 -0800

    added Theme changer

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Toolbar from './Theme';
 
 export default class Header extends Component {
   render() {
     return (
       <header>
+        <Toolbar />
         <Link to={'/'}>
           <button>home</button>
         </Link>
