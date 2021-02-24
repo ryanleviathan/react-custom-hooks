a4c240fb294548cd830e1c84492d42bad7b39da2
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-24 13:50:24 -0800

    added themes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,14 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AllQuotes from '../../containers/AllQuotes';
 import ByCharacterQuotes from '../../containers/ByCharacterQuotes';
 import Header from './Header';
-import Toolbar, { ThemeContext } from './Theme';
 
 export default function App() {
   return (
     <Router>
-      <ThemeContext.Provider value={themes.dark}>
-        <Toolbar />
-      </ThemeContext.Provider>
       <Header />
       <Switch>
         <Route exact path="/" component={AllQuotes} />       
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router } from 'react-router-dom';
 import App from './components/app/App';
-import history from './components/app/history';
 
 render(
-  <Router history={history}>
+  <Router>
     <App />
   </Router>,
   document.getElementById('root')
